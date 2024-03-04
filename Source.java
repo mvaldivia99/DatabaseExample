50ec20c02f2c5107fee6c02b1a1d1f2b1553510d
Valdivia <MichellV@bigbend.edu>
2024-03-04 15:29:30 -0800

    Added ArrayList String Example
    Added example showing ArrayLists with Strings, using the add and remove methods.

--- Source.java
+++ Source.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 // Database Lab 5 Example
 class Product{
     // attributes - what makes up an object/class
@@ -23,15 +25,30 @@ class Product{
 public class Source {
     public static void main(String[] args) {
         // this will be a table in my Database
-        Product[] table = new Product[3];
+        /*Product[] table = new Product[3];
 
         table[0] = new Product(134, "Chai", 13.5);
         table[1] = new Product(256, "Syrup", 7.5);
-        table[2] = new Product(382, "Cajun Seasoning", 16.5);
+        table[2] = new Product(382, "Cajun Seasoning", 16.5);*/
 
-        // enhanced for-loop
-        for (Product row : table){
-            System.out.println(row.toString());
-        }
+        // ArrayList of products
+        //ArrayList<Product> myProd = new ArrayList<Product>();
+
+
+        ArrayList<String> strList = new ArrayList<String>();
+        // add a new string to the strList ArrayList
+        strList.add("hello"); // 0
+        strList.add("world"); // 1
+        strList.add("to all!"); // 2
+        strList.add(1, "!");
+        strList.add("hello"); // 4
+
+        // remove by element
+        strList.remove("hello"); // remove the hello at 0
+
+        // remove by index
+        strList.remove(0);
+
+        
     }
 }
